1c3ec1e1cc8ca474c6cbd28ffce93d28eb32e9a4
kevin <570279336@qq.com>
2020-02-16 14:13:19 +0800

    优化

--- src/main/java/com/example/demo/sbSaCaCb/SbSaCaCb.java
+++ src/main/java/com/example/demo/sbSaCaCb/SbSaCaCb.java
@@ -7,6 +7,10 @@ public class SbSaCaCb {
     public static void main(String[] args) {
         A a = new A();
 //        B b = new B();
+        Integer c= 200;
+        Integer d = 200;
+        System.out.println(c.equals(d));
+
     }
 
 }
